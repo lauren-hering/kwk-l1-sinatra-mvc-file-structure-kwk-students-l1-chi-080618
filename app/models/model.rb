c15d1f5516776151f390e5e6c66d227b7afd1c16
lauren-hering <lauren.hering@comcast.net>
2018-08-14 21:08:20 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,16 @@
-class Model
+class Dog
   # Replace with Dog clas
+attr_accessor :name, :breed, :age
+DOGS = []
+def initialize(name, breed, age)
+  @name = name
+  @breed = breed
+  @age = age
+DOGS << self
+
+end
+
+def self.assert_select_all
+  DOGS 
+end
 end
